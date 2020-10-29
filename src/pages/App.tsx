385737e2625788aa925b75f544116cf257e9d2ef
EduardoColissi <educolissi@hotmail.com>
2020-10-28 21:36:26 -0300

    Feat: Added Pokemon Name, Previous and Next Buttons

---
+++ src/pages/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import api from '../services/api';
+
+interface IPokemon {
+  name: string
+}
+
+function App() {
+  const [pokemon, setPokemon] = useState<IPokemon>({name: ''});
+  const [pokeIndex, setPokeIndex] = useState<number>(1);
+
+  useEffect(() => {
+    api.get(`pokemon/${pokeIndex}`)
+    .then(result => {
+      setPokemon(result.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, [pokeIndex]);
+
+  return (
+    <div className="App">
+      <h1>{pokemon.name}</h1>
+
+      <Button type="primary" onClick={() => {
+        setPokeIndex(pokeIndex - 1);
+      }}>Anterior</Button>
+      <Button type="primary" onClick={() => setPokeIndex(pokeIndex + 1)}>Próximo</Button>
+    </div>
+  );
+}
+
+export default App;
